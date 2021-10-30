d4349a1106675afae8f1d3e9bdb466289dc6e50b
Tiago Tito <48845835+titotiago@users.noreply.github.com>
2021-10-30 16:20:27 -0300

    feat: adds login screen

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import { AuthRoutes } from '@/modules/auth';
 import { Landing } from '@/modules/misc';
+import { Surveys } from '@/modules/surveys';
 
 export const PublicRoutes = () => {
   return (
     <Routes>
       <Route path="/*" element={<AuthRoutes />} />
       <Route path="/" element={<Landing />} />
+      <Route path="/enquetes" element={<Surveys />} />
     </Routes>
   );
 };
